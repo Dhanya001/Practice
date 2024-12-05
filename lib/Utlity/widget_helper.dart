1b63fb41afb617d0d6e10637db182cd7e44bbf4a
Dhanya001 <149471022+Dhanya001@users.noreply.github.com>
2024-12-05 20:52:14 +0530

    Update widget_helper.dart

--- lib/Utlity/widget_helper.dart
+++ lib/Utlity/widget_helper.dart
@@ -1,76 +1,167 @@
-import 'package:flutter/material.dart';
+onPressed: () async {
+  if (_formKey.currentState?.validate() ?? false) {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('Phone', _phoneController.text);
+    
+    bool isInternetConnected = await constant.isInternet();
+    if (isInternetConnected) {
+      constant.showLoading(context);
+      try {
+        if (_userEmail != null) {
+          var response = await GlobalHelper().login(
+            _phoneController.text.trim(),
+            _userEmail!,
+          );
 
-class MySmallText extends StatelessWidget {
-  final String title;
-  final Color color;
-  final bool isBold;
-
-  const MySmallText({
-    Key? key,
-    required this.title,
-    this.color = Colors.black,
-    this.isBold = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 14,
-        color: color,
-        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
-      ),
-    );
+          if (response != null && response['success'] == true) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => OtpScreen()),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Login failed. Please try again.')),
+            );
+          }
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Please Sign in E-mail")),
+          );
+        }
+      } catch (e) {
+        Navigator.of(context).pop();
+        setState(() {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.toString())),
+          );
+        });
+      }
+    } else {
+      constant.showErrorDialog(
+        context,
+        'Internet Error!',
+        'Please check your internet connection',
+        icon: Icon(
+          Icons.wifi_off_rounded,
+          color: Colors.black38,
+        ),
+      );
+    }
   }
 }
 
-class MyMediumText extends StatelessWidget {
-  final String title;
-  final Color color;
-  final bool isBold;
 
-  const MyMediumText({
-    Key? key,
-    required this.title,
-    this.color = Colors.black,
-    this.isBold = false,
-  }) : super(key: key);
+SendOTP(String otp) async {
+    print('Sending : otp=$otp,');
+    try {
+      var response = await http.post(
+        Uri.parse('${constant.apiLocalName}/verifyOtp'),
+		 headers: {
+    'Content-Type': 'application/json', // Specify the content type
+  },
+        body: {
+          'otp': otp,
+        },
+      );
+      print('resoponse: ${response}');
+      print('status: ${response.statusCode}');
+      print('body: ${response.body}');
 
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 18,
-        color: color,
-        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
-      ),
-    );
+      if (response.statusCode == 200) {
+        var responseData = jsonDecode(response.body);
+        print('data: $responseData');
+      } else {
+        print('error: ${response.reasonPhrase}');
+        print('Response body: ${response.body}');
+        throw Exception(': ${response.statusCode}');
+      }
+    }catch(e){
+      print('Error during Otp authentication: $e');
+      rethrow;
+    }
   }
+
+
+Future<void> _sendOtpToEmail(String? email) async {
+    final int otp = 100000 + Random().nextInt(900000); // Generate a 6-digit OTP
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('otp', otp); // Store OTP as an int
+    await prefs.setString('userEmail', email!);
+
+    print('Sending OTP: $otp to $email');
 }
 
-class MyBigText extends StatelessWidget {
-  final String title;
-  final Color color;
-  final bool isBold;
-
-  const MyBigText({
-    Key? key,
-    required this.title,
-    this.color = Colors.black,
-    this.isBold = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 24,
-        color: color,
-        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
-      ),
-    );
-  }
+Future<void> _loadPhoneNumber() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+        _phoneNumber = prefs.getString('Phone');
+        // Retrieve OTP as an int
+        int? otp = prefs.getInt('otp');
+        print('Loaded OTP: $otp'); // Debug print
+    });
 }
+
+SendOTP(int otp) async {
+    print('Sending : otp=$otp,');
+    try {
+        var response = await http.post(
+            Uri.parse('${constant.apiLocalName}/verifyOtp'),
+            headers: {
+                'Content-Type': 'application/json', // Specify the content type
+            },
+            body: jsonEncode({'otp': otp.toString()}), // Convert OTP to string for JSON
+        );
+        print('response: ${response}');
+        print('status: ${response.statusCode}');
+        print('body: ${response.body}');
+
+        if (response.statusCode == 200) {
+            var responseData = jsonDecode(response.body);
+            print('data: $responseData');
+        } else {
+            print('error: ${response.reasonPhrase}');
+            print('Response body: ${response.body}');
+            throw Exception(': ${response.statusCode}');
+        }
+    } catch (e) {
+        print('Error during Otp authentication: $e');
+        rethrow;
+    }
+}
+
+SizedBox(
+    width: double.infinity,
+    child: MyBottomButton(
+        title: "Continue",
+        onPressed: () async {
+            bool isInternetConnected = await constant.isInternet();
+            if (isInternetConnected) {
+                try {
+                    int otp = int.parse(_otpController.text); // Convert input to int
+                    var response = await GlobalHelper().SendOTP(otp); // Send OTP as int
+                    if (response != null && response['success'] == true) {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => AllowLocationPage()),
+                        );
+                    } else {
+                        setState(() {
+                            _invalidOtp = true;
+                        });
+                    }
+                } catch (e) {
+                    setState(() {
+                        _invalidOtp = true; // Show invalid OTP message
+                    });
+                }
+            } else {
+                constant.showErrorDialog(context, 'Internet Error!',
+                    'Please check your internet connection',
+                    icon: Icon(
+                        Icons.wifi_off_rounded,
+                        color: Colors.black38,
+                    ));
+            }
+        },
+    ),
+),
