d3a5497785d1f2b4343983b92178651e57ebc5e2
Dhanya001 <149471022+Dhanya001@users.noreply.github.com>
2024-12-30 13:56:08 +0530

    Update Rate_card.dart

--- lib/Home_Screens/Rate_card.dart
+++ lib/Home_Screens/Rate_card.dart
@@ -1,197 +1,238 @@
-void initialPay(
-    BuildContext context,
-    String sessionId,
-    String orderId,
-    String amount,
-    String studentId,
-    String cf_id,
-    String currentYear,
-    String installments,
-    String installment_id,
-    Function onPaymentSuccess
-    ) {
-  var session = CFSessionBuilder()
-      .setEnvironment(CFEnvironment.SANDBOX)
-      .setOrderId(orderId)
-      .setPaymentSessionId(sessionId)
-      .build();
-  var cfWebCheckout = CFWebCheckoutPaymentBuilder().setSession(session).build();
-  var cfPaymentGateway = CFPaymentGatewayService();
-  cfPaymentGateway.setCallback(
-    (p0) async{
-      // InitCashFreePayment().orderStatus(orderId);
-       onPaymentSuccess();
+import 'dart:developer';
+import 'package:eschoolapp/models/payment_history_model.dart';
+import 'package:eschoolapp/screen/payment_receipt.dart';
+import 'package:uuid/uuid.dart';
+import 'package:eschoolapp/screen/fees_history_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hexcolor/hexcolor.dart';
+import '../models/student_model.dart';
+import '../utils/cash_fees_gateway_payment.dart';
+import '../utils/global_helper.dart';
+import '../widget_helper/widgets_helper.dart';
+import 'package:eschoolapp/utils/constants.dart' as constants;
+
+var uuid = const Uuid();
+
+class FeesScreen extends StatefulWidget {
+  final String studentId;
+  final String currentYear;
+  final StudentModel studentModel;
+  const FeesScreen(
+      {super.key,
+      required this.studentId,
+      required this.currentYear,
+      required this.studentModel});
   
-      await GlobalHelper().paymentResponse(
-          orderId, studentId, currentYear, installments, installment_id);
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PaymentSuccessful(
-              amount: amount,
-              transactionId: '4849398439static',
-             isSuccessStatus: true,
-            ),
-          ));
-
-      print('Payment successful p0: $p0}');
-      print('Payment successful cf_order_id: $cf_id}');
-      print('Payment successful sessionId: $sessionId}');
-      print('Payment successful orderId: $orderId}');
-      print('Payment successful amount: $amount}');
-      print('Payment successful studentId: $studentId}');
-    },
-    (p0, p1) async {
-      // await InitCashFreePayment().orderStatus(cf_id);
-
-     await GlobalHelper().paymentResponse(
-          orderId, studentId, currentYear, installments, installment_id);
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PaymentSuccessful(
-              amount: amount,
-              transactionId: '4849398439static',
-              isSuccessStatus: false,
-            ),
-          ));
-      print('Payment failed p0.getMessage(): ${p0.getMessage()}');
-      print('Payment failed p0.getCode(): ${p0.getCode()}');
-      print('Payment failed p0.getStatus(): ${p0.getStatus()}');
-      print('Payment failed p0.getType(): ${p0.getType()}');
-
-      print('Payment failed cf_order_id: $cf_id');
-      print('Payment failed p1: $p1');
-      print('Payment failed sessionId: $sessionId}');
-      print('Payment failed orderId: $orderId}');
-      print('Payment failed amount: $amount}');
-      print('Payment failed studentId: $studentId}');
-    },
-  );
-  cfPaymentGateway.doPayment(cfWebCheckout);
+  @override
+  State<FeesScreen> createState() => _FeesScreenState();
 }
 
-                                            InitCashFreePayment().createOrder(
-                                            context,
-                                            paymentInitialStoreInfoResponse[
-                                                'reference_no'],
+class _FeesScreenState extends State<FeesScreen> {
+  List<Map<String, dynamic>> feesDetails = [];
+  List<PaymentHistoryModel> paymentHistoryModel = [];
 
-                                            paymentInitialStoreInfoResponse[
-                                            'total_fees'],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: constants.backgroundColor,
+        appBar: appbar(context, 'Fees', 'Installment'),
+        floatingActionButton: FloatingActionButton(onPressed: () async {
+          var feedetails1 = await GlobalHelper().feeDetails(widget.studentId);
+          print('Dhnanajay');
+          print(feedetails1);
+        }),
+        body: FutureBuilder(
+          future: GlobalHelper().feeDetails(widget.studentId),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                paymentHistoryModel = snapshot.data as List<PaymentHistoryModel>;
+                List<dynamic> feesDataList = [];
+                Map<String, dynamic> mapData = snapshot.data as Map<String, dynamic>;
+                feesDataList = mapData['installments'] as List<dynamic>;
 
-                                            widget.studentId,
-                                            "${widget.studentModel.name} ${widget.studentModel.fatherName} ${widget.studentModel.surname}",
-                                            widget.studentModel.fatherEmail!,
-                                            widget.studentModel.fatherNumber
-                                                .toString(),
-                                            feesDetails[index][
-                                                    'online_admission_fees_id']
-                                                .toString(),
-                                            feesDetails[index]['installment']
-                                                .toString(),
-                                            widget.currentYear,
-                                            (){
-                                              setState(() {});
-                                            }
-                                            );
-											
-
-
-
------------------------------------------
-
-  
-void initialPay(
-    BuildContext context,
-    String sessionId,
-    String orderId,
-    String amount,
-    String studentId,
-    String cf_id,
-    String currentYear,
-    String installments,
-    String installment_id) {
-  var session = CFSessionBuilder()
-      .setEnvironment(CFEnvironment.SANDBOX)
-      .setOrderId(orderId)
-      .setPaymentSessionId(sessionId)
-      .build();
-  var cfWebCheckout = CFWebCheckoutPaymentBuilder().setSession(session).build();
-  var cfPaymentGateway = CFPaymentGatewayService();
-  cfPaymentGateway.setCallback(
-    (p0) async{
-      String actualTransactionId = cf_id;
-       String actualAmount = amount;
-      // InitCashFreePayment().orderStatus(orderId);
-      await GlobalHelper().paymentResponse(
-          orderId, studentId, currentYear, installments, installment_id);
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PaymentSuccessful(
-              amount: actualAmount,
-              transactionId: actualTransactionId,
-             isSuccessStatus: true,
-            ),
-          ));
-
-      print('Payment successful p0: $p0}');
-      print('Payment successful cf_order_id: $cf_id}');
-      print('Payment successful sessionId: $sessionId}');
-      print('Payment successful orderId: $orderId}');
-      print('Payment successful amount: $amount}');
-      print('Payment successful studentId: $studentId}');
-    },
-    (p0, p1) async {
-      // await InitCashFreePayment().orderStatus(cf_id);
-
-     await GlobalHelper().paymentResponse(
-          orderId, studentId, currentYear, installments, installment_id);
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => PaymentSuccessful(
-              amount: actualAmount,
-              transactionId: 'Transaction failed',
-              isSuccessStatus: false,
-            ),
-          ));
-      print('Payment failed p0.getMessage(): ${p0.getMessage()}');
-      print('Payment failed p0.getCode(): ${p0.getCode()}');
-      print('Payment failed p0.getStatus(): ${p0.getStatus()}');
-      print('Payment failed p0.getType(): ${p0.getType()}');
-
-      print('Payment failed cf_order_id: $cf_id');
-      print('Payment failed p1: $p1');
-      print('Payment failed sessionId: $sessionId}');
-      print('Payment failed orderId: $orderId}');
-      print('Payment failed amount: $amount}');
-      print('Payment failed studentId: $studentId}');
-    },
-  );
-  cfPaymentGateway.doPayment(cfWebCheckout);
+                return SingleChildScrollView(
+                  physics: ScrollPhysics(),
+                  child: Column(
+                    children: [
+                      const HeaderRow(boxShadow: true, title: "Installment"),
+                      Gap(15),
+                      ListView.builder(
+                        itemCount: feesDataList.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          feesDetails.add(feesDataList[index]['\u0000yii\\db\\BaseActiveRecord\u0000_attributes']);
+                          return feesDetails[index]['total_fees'] == 0
+                              ? Container()
+                              : Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: InstallmentsWidget(
+                                    callback: () async {
+                                      // Directly navigate to PaymentReceipt
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => PaymentReceipt(
+                                            paymentHistoryModel: paymentHistoryModel[index],
+                                            studentModel: widget.studentModel,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    title: "Installment ${feesDetails[index]['installment']}",
+                                    paid: feesDetails[index]['total_fees'].toString(),
+                                    due: mapData['balance_fees'].toString(),
+                                    btn: "View Receipt",
+                                    color: HexColor('#135E9E'),
+                                    paymentHistoryModel: paymentHistoryModel[index],
+                                    studentModel: widget.studentModel,
+                                  ),
+                                );
+                        },
+                      ),
+                      const Gap(20),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: MyContainer(
+                          top: 10,
+                          bottom: 10,
+                          left: 10,
+                          right: 10,
+                          boxShadow: true,
+                          child: Column(
+                            children: [
+                              const HeaderRow(title: "Fees Information"),
+                              const Gap(15),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  FeesColumn(
+                                    firstVal: "Total Fees",
+                                    secondVal: mapData['total_fees'].toString(),
+                                  ),
+                                  FeesColumn(
+                                    firstVal: "Total Paid",
+                                    secondVal: mapData['paid_fees'].toString(),
+                                  ),
+                                  FeesColumn(
+                                    firstVal: "Total Balance",
+                                    secondVal : mapData['balance_fees'].toString(),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const Gap(10),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(child: Text('No Data'));
+              }
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
+  }
 }
 
-InitCashFreePayment().createOrder(
-                                            context,
-                                            paymentInitialStoreInfoResponse[
-                                                'reference_no'],
+class InstallmentsWidget extends StatelessWidget {
+  final String title;
+  final String paid;
+  final String due;
+  final String btn;
+  final Color color;
+  final VoidCallback callback;
+  final PaymentHistoryModel paymentHistoryModel;
+  final StudentModel studentModel;
 
-                                            paymentInitialStoreInfoResponse[
-                                            'total_fees'],
+  const InstallmentsWidget({
+    super.key,
+    required this.title,
+    required this.paid,
+    required this.due,
+    required this.btn,
+    required this.color,
+    required this.callback,
+    required this.paymentHistoryModel,
+    required this.studentModel,
+  });
 
-                                            widget.studentId,
-                                            "${widget.studentModel.name} ${widget.studentModel.fatherName} ${widget.studentModel.surname}",
-                                            widget.studentModel.fatherEmail!,
-                                            widget.studentModel.fatherNumber
-                                                .toString(),
-                                            feesDetails[index][
-                                                    'online_admission_fees_id']
-                                                .toString(),
-                                            feesDetails[index]['installment']
-                                                .toString(),
-                                            widget.currentYear);
-setState(() {
-                                              feesDetails.clear(); 
-                                               });
+  @override
+  Widget build(BuildContext context) {
+    return MyContainer(
+      right: 10,
+      left: 10,
+      top: 10,
+      bottom: 10,
+      height: 200,
+      boxShadow: true,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              MyTextMedium(
+                title: title,
+                color: HexColor("#0C6992"),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FeesColumn(
+                firstVal: paid,
+                secondVal: "Total Paid",
+              ),
+              Container(
+                height: 100,
+                child: VerticalDivider(
+                  thickness: 3.5,
+                  color: HexColor('#ABADAE'),
+                ),
+              ),
+              FeesColumn(
+                firstVal: due,
+                secondVal: "Total Due",
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            child: MyTextButton(
+              callback: callback,
+              radius: 5,
+              bgColor: color,
+              myWidget: MyTextMedium(
+                title: btn,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class FeesColumn extends StatelessWidget {
+  final String firstVal;
+  final String secondVal;
+  const FeesColumn({super.key, required this.firstVal, required this.secondVal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyTextSmall(title: firstVal, color: HexColor('#646668')),
+        Gap(5),
+        MyTextSmall(title: secondVal, color: HexColor('#646668')),
+      ],
+    );
+  }
+}
