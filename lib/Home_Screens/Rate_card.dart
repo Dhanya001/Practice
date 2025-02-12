9119e49bc95026097a391eaebf454331b9a2793a
Dhanya001 <149471022+Dhanya001@users.noreply.github.com>
2025-02-12 14:50:05 +0530

    Update Rate_card.dart

--- lib/Home_Screens/Rate_card.dart
+++ lib/Home_Screens/Rate_card.dart
@@ -1,132 +1,68 @@
-@override
-void initState() {
-  super.initState();
-  initial();
-  print('Testing');
-
-  // Initialize SelectedItems based on existingScheduleMap
-  if (widget.existingScheduleMap['schedule_items'] != null) {
-    for (var item in widget.existingScheduleMap['schedule_items']) {
-      RateItemModel? foundItem = rateItemModel?.firstWhere(
-        (rateItem) => rateItem.rateItemsName == item['product_name'],
-        orElse: () => null,
-      );
-      if (foundItem != null) {
-        SelectedItems.add(foundItem);
-      }
-    }
+Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => UpdateProfileScreen(
+      studentModel: widget.studentModel,
+      std: widget.std,
+      studentId: widget.studentId,
+    ),
+  ),
+).then((updatedData) {
+  if (updatedData != null) {
+    setState(() {
+      // Assuming updatedData is a Map<String, dynamic>
+      widget.studentModel = StudentModel.fromJson(updatedData);
+    });
   }
+});
+
+if (response['success'] == true) {
+  var studentUpdatedData =
+      await GlobalHelper().getStudentInfo(context, widget.studentId!);
+  
+  // Pass the updated data back to the ProfileScreen
+  Navigator.pop(context, studentUpdatedData);
 }
 
-
-@override
-Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
-        childAspectRatio: 2 / 3,
-      ),
-      itemCount: widget.rateItemModel.length,
-      itemBuilder: (context, index) {
-        bool isSelected = widget.selectedlist.contains(widget.rateItemModel[index]);
-
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              if (isSelected) {
-                widget.selectedlist.remove(widget.rateItemModel[index]);
-              } else {
-                widget.selectedlist.add(widget.rateItemModel[index]);
-              }
-            });
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: widget.rateItemModel[index].rateItemsImage == null
-                      ? Container(color: Colors.white)
-                      : Container(
-                          margin: const EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            image: DecorationImage(
-                              image: NetworkImage('${widget.rateItemModel[index].rateItemsImage}'),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: SizedBox(
-                          height: 40,
-                          child: Text(
-                            widget.rateItemModel[index].rateItemsName ?? '',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 15,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            if (isSelected) {
-                              widget.selectedlist.remove(widget.rateItemModel[index]);
-                            } else {
-                              widget.selectedlist.add(widget.rateItemModel[index]);
-                            }
-                          });
-                        },
-                        child: isSelected
-                            ? Container(
-                                height: 20,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).primaryColor,
-                                  shape: BoxShape.circle,
-                                  border: Border.all(color: Colors.black),
-                                ),
-                                child: Icon(
-                                  Icons.check,
-                                  size: 15,
-                                  color: Colors.white,
-                                ),
-                              )
-                            : Container(
-                                height: 20,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(color: Colors.black),
-                                ),
-                              ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => UpdateProfileScreen(
+      studentModel: widget.studentModel,
+      std: widget.std,
+      studentId: widget.studentId,
     ),
-  );
+  ),
+).then((updatedData) {
+  if (updatedData != null) {
+    setState(() {
+      // Update the studentModel with the new data
+      widget.studentModel = updatedData;
+    });
+  }
+});
+
+// In ProfileScreen
+Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => UpdateProfileScreen(
+      studentModel: widget.studentModel,
+      std: widget.std,
+      studentId: widget.studentId,
+    ),
+  ),
+).then((updatedData) {
+  if (updatedData != null) {
+    setState(() {
+      widget.studentModel = updatedData; // Update the model with new data
+    });
+  }
+});
+
+// In UpdateProfileScreen
+if (response['success'] == true) {
+  var studentUpdatedData =
+      await GlobalHelper().getStudentInfo(context, widget.studentId!);
+  Navigator.pop(context, studentUpdatedData); // Pass updated data back
 }
